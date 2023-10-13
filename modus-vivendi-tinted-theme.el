eae895ec4489543384adac3c5d8a42c9e973e160
Lucius <chenyh572@gmail.com>
2023-10-14 00:15:16 +0800

    Update modus-vivendi-tinted-theme.el

--- modus-vivendi-tinted-theme.el
+++ modus-vivendi-tinted-theme.el
@@ -160,10 +160,10 @@ which corresponds to a minimum contrast in relative luminance of
       (bg-char-1 "#7f1f7f")
       (bg-char-2 "#625a00")
 
-      (bg-mode-line-active        "#48674e")
+      (bg-mode-line-active        "#325d49")
       (fg-mode-line-active        "#ffffff")
       (border-mode-line-active    "#979797")
-      (bg-mode-line-inactive      "#292d48")
+      (bg-mode-line-inactive      "#143325")
       (fg-mode-line-inactive      "#969696")
       (border-mode-line-inactive  "#606270")
 
