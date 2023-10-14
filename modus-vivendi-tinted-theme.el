09c15308e4559875b3f86c44d63860f8a41cf9d0
Lucius <chenyh572@gmail.com>
2023-10-14 23:13:51 +0800

    Update modus-vivendi-tinted-theme.el

--- modus-vivendi-tinted-theme.el
+++ modus-vivendi-tinted-theme.el
@@ -417,7 +417,7 @@ which corresponds to a minimum contrast in relative luminance of
 ;;;; Heading mappings
 
       (fg-heading-0 red-cooler)
-      (fg-heading-1 cyan-cooler)
+      (fg-heading-1 magenta-faint)
       (fg-heading-2 yellow-faint)
       (fg-heading-3 blue-faint)
       (fg-heading-4 magenta)
