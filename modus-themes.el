cdad29eb164a7f337fdad1398aa62c3a98c25f2e
Lucius <chenyh572@gmail.com>
2023-10-14 20:31:29 +0800

    Update modus-themes.el

--- modus-themes.el
+++ modus-themes.el
@@ -3378,6 +3378,16 @@ FG and BG are the main colors."
     `(org-habit-overdue-future-face ((,c :background ,bg-graph-red-1)))
     `(org-habit-ready-face ((,c :background ,bg-graph-green-0 :foreground "black"))) ; fg is special case
     `(org-habit-ready-future-face ((,c :background ,bg-graph-green-1)))
+;;;; org-modern
+    `(org-modern-date-active ((,c :inherit (ef-themes-fixed-pitch org-modern-label) :background ,bg-alt)))
+    `(org-modern-date-inactive ((,c :inherit (ef-themes-fixed-pitch org-modern-label) :background ,bg-dim :foreground ,fg-dim)))
+    `(org-modern-done ((,c :inherit org-modern-label :background ,bg-info :foreground ,info)))
+    `(org-modern-priority ((,c :inherit (org-modern-label org-priority) :background ,bg-dim)))
+    `(org-modern-statistics ((,c :inherit org-modern-label :background ,bg-dim)))
+    `(org-modern-tag ((,c :inherit (org-modern-label org-tag) :background ,bg-dim)))
+    `(org-modern-time-active ((,c :inherit (ef-themes-fixed-pitch org-modern-label) :background ,bg-active :foreground ,fg-intense)))
+    `(org-modern-time-inactive ((,c :inherit (org-modern-label org-modern-date-inactive))))
+    `(org-modern-todo ((,c :inherit org-modern-label :background ,red-warmer :foreground ,err)))
 ;;;;; org-journal
     `(org-journal-calendar-entry-face ((,c :inherit modus-themes-slant :foreground ,date-common)))
     `(org-journal-calendar-scheduled-face ((,c :inherit (modus-themes-slant org-scheduled))))
