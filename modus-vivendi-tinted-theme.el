1288d9475361992c5d0c36f6d5c6e8f0080f9de5
Lucius <chenyh572@gmail.com>
2023-10-14 12:28:23 +0800

    Update modus-vivendi-tinted-theme.el

--- modus-vivendi-tinted-theme.el
+++ modus-vivendi-tinted-theme.el
@@ -63,7 +63,7 @@ which corresponds to a minimum contrast in relative luminance of
       (fg-dim           "#989898")
       (fg-alt           "#c6daff")
       (bg-active        "#1d282a")
-      (bg-inactive      "#1a2324")
+      (bg-inactive      "#192324")
       (border           "#376751")
 
 ;;; Common accent foregrounds
