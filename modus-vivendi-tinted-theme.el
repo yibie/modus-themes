33ab3c25205d950a99f5c8691fedc472511bd684
Lucius <chenyh572@gmail.com>
2023-10-16 21:45:08 +0800

    Update modus-vivendi-tinted-theme.el

--- modus-vivendi-tinted-theme.el
+++ modus-vivendi-tinted-theme.el
@@ -175,7 +175,7 @@ which corresponds to a minimum contrast in relative luminance of
       (modeline-info    "#9fefff")
 
       (bg-tab-bar                  bg-main)
-      (bg-tab-current               bg-dim)
+      (bg-tab-current bg-mode-line-inactive)
       (bg-tab-other                bg-main)
 
 ;;; Diffs
