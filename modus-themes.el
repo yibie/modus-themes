a205865c053a2a6df6b6a5dcf3537b5e013f9d3c
Lucius <chenyh572@gmail.com>
2023-10-28 20:43:33 +0800

    Update modus-themes.el

--- modus-themes.el
+++ modus-themes.el
@@ -3710,7 +3710,7 @@ FG and BG are the main colors."
     `(tab-bar-tab-group-current ((,c :inherit bold :background ,bg-tab-current :box (:line-width -2 :color ,bg-tab-current) :foreground ,fg-alt)))
     `(tab-bar-tab-group-inactive ((,c :background ,bg-tab-bar :box (:line-width -2 :color ,bg-tab-bar) :foreground ,fg-alt)))
     `(tab-bar-tab ((,c :inherit bold :box (:line-width -2 :color ,bg-tab-current) :background ,bg-tab-current :foreground ,olive :underline (:style line :color ,olive))))
-    `(tab-bar-tab-inactive ((,c :box (:line-width -2 :color ,bg-tab-other) :background "gray50")))
+    `(tab-bar-tab-inactive ((,c :box (:line-width -2 :color "gray50") :background ,bg-tab-other)))
     `(tab-bar-tab-ungrouped ((,c :inherit tab-bar-tab-inactive)))
 ;;;;; tab-line-mode
     `(tab-line ((,c :inherit modus-themes-ui-variable-pitch :background ,bg-tab-bar :height 0.95)))
