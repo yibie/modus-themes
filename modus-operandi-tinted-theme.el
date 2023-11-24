0c04d492c1ee229019a230bf77e64a47e0833dbe
Lucius <chenyh572@gmail.com>
2023-11-24 10:24:15 +0800

    Update modus-operandi-tinted-theme.el

--- modus-operandi-tinted-theme.el
+++ modus-operandi-tinted-theme.el
@@ -84,7 +84,7 @@ which corresponds to a minimum contrast in relative luminance of
       (yellow-warmer   "#b6540f")
       (yellow-cooler   "#ae5a30")
       (yellow-faint    "#876450")
-      (yellow-intense  "#808000")
+      (yellow-intense  "#cfcf00")
       (blue            "#375cc6")
       (blue-warmer     "#5f5fdf")
       (blue-cooler     "#265fbf")
